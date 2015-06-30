4e6c4e148a6ad468f61e17400ba093a590858aae
s14004 <s14004@std.it-college.ac.jp>
2015-06-30 13:58:57 +0900

    Initial commit

---
+++ src/jp/ac/it_college/std/s14004/Main.java
@@ -0,0 +1,44 @@
+package jp.ac.it_college.std.s14004;
+
+import jp.ac.it_college.std.nakasone.lib.exam.Communicator;
+import jp.ac.it_college.std.nakasone.lib.exam.Communicator.InputListener;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by s14003 on 15/06/30.
+ */
+
+public class Main {
+    public static void main(String... args) {
+        Communicator comm = new Communicator((msg) -> {
+            System.out.println(msg);
+        });
+
+        comm.start();
+        String name = "s14004:";
+
+        while(true) {
+            Scanner stdIn = new Scanner(System.in);
+            System.out.println("メッセージを入力してください");
+            String message = stdIn.next();
+            if (message.equals("exit")) {
+                try {
+                    comm.send(name + "切断しました");
+                    break;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                try {
+                    comm.send(name + message);
+                } catch (IOException var3) {
+                    var3.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+}
